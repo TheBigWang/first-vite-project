b72af598603562cca2619c5b6faa8f26ae1cf227
Harry <harrythomasmitchell@gmail.com>
2024-06-11 12:00:49 +0100

    added simulated waiting time to load data

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,63 @@
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import './App.css';
 import Page from './components/page/Page.tsx';
 import { IButton } from './components/page/nav/Button.tsx';
 import Nav from './components/page/nav/Nav.tsx';
-import Data from './data/data.json';
 
+import { IDataItem, load } from "./server";
 
+/**function with whole app */
 const App = () => {
-  const [page, setPage] = useState('capybara')
+  /** Data */
+  const [data, setData] = useState<IDataItem[]>();
 
+  /** Load data */
+  useEffect(() => {
+    (async () => {
+      const data = await load();
+      setData(data);
+      console.log(data[0].id)
+    })()
+  }, [])
+  
+  // set page variable to start as 'capybara'
+  const [page, setPage] = useState('capybara')
+  
+  // when click, set page variable to be id of button 
   const onHandleClick = (id: string) => {
     setPage(id)
   }
-
+  
+  //creating nav bar data variable using memo that checks if data has been loaded and then maps through json creating buttons
   const navData = useMemo(() => {
-    const data: Omit<IButton, 'onSelect'>[] = Data.map((item) => {
+    if (!data) {
+      return
+    }
+
+    const navData: Omit<IButton, 'onSelect'>[] = data.map((item) => {
       return {
         id: item.id,
         label: item.label
       }
     })
 
-    return data;
-  }, [])
+    return navData;
+  }, [data])
 
+
+  useEffect(() => {
+    console.log(navData)
+  }, [navData])
+
+  
+  // creating page display variable using memo that reads through json and displays page using data
   const pageDisplay = useMemo(() => {
-    const pageData = Data.find((item) => {
+
+    if (!data) {
+      return
+    }
+
+    const pageData = data.find((item) => {
       return page === item.id;
     })
     if (pageData) {
@@ -42,13 +74,21 @@ const App = () => {
     } else {
       return null
     }
-  }, [page])
+  }, [page,data])
 
+  
+  useEffect(() => {
+    console.log(pageDisplay)
+  }, [pageDisplay])
 
+  
+  // return statement displaying everything
   return (
     <>
-      <div className='button_row'>|
-        <Nav buttonData={navData} onSelect={onHandleClick}></Nav>
+      <div className='button_row'>
+        {navData && (
+          <Nav buttonData={navData} onSelect={onHandleClick}></Nav>
+        )}
       </div>
       {pageDisplay}
     </>
---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import Data from './data/data.json';
+
+export interface IDataItem {
+    id: string;
+    label : string,
+    title: string,
+    header: string,
+    text: string,
+    img: string,
+
+}
+
+export const load = () => {
+    const DELAY = 2000;
+
+    return new Promise<IDataItem[]>((resolve) => {
+        setTimeout(() => {
+            resolve(Data as IDataItem[]);
+        }, DELAY)
+    })
+}
